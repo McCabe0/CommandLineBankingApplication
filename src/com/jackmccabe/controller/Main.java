6bc79e8b0bbcb559fc2c58a879979b567a6ea637
Jack M <jackmccabe1995@hotmail.co.uk>
2020-06-21 20:34:36 +0100

    Created a view file

--- src/com/jackmccabe/controller/Main.java
+++ src/com/jackmccabe/controller/Main.java
@@ -7,5 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+
+        BankAccount bankAccount = new BankAccount(new Customer("Jack", "McCabe"));
+
+        bankAccount.addingMoneyToAccount(1);
+
+
     }
 }
