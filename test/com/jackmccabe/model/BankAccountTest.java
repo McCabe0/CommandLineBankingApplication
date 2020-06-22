c422196e16b1c633fcd94ef34c11bab4b9162bcb
Jack M <jackmccabe1995@hotmail.co.uk>
2020-06-22 18:56:47 +0100

    Added tests

--- test/com/jackmccabe/model/BankAccountTest.java
+++ test/com/jackmccabe/model/BankAccountTest.java
@@ -1,6 +1,7 @@
 package com.jackmccabe.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +14,42 @@ class BankAccountTest {
         bankAccount = new BankAccount(new Customer("Jack", "McCabe"));
     }
 
+    @Test
+    public void ifAccountDepositsRight() throws Exception{
+        bankAccount.addingMoneyToAccount(10);
+
+        int amount = bankAccount.getCustomerCurrentBankAccount();
+
+        assertEquals(10, amount);
+    }
+
+    @Test
+    public void ifAccountCreditCorrectly() throws Exception{
+        bankAccount.addingMoneyToAccount(10);
+
+         bankAccount.creditingMoneyFromTheAccount(5);
+         int amount = bankAccount.getCustomerCurrentBankAccount();
+
+         assertEquals(5, amount);
+    }
+
+    @Test
+    public void displaysTransactionHistoryDeposit() throws Exception{
+        bankAccount.addingMoneyToAccount(10);
+
+        int deposit = bankAccount.getDepositTransactions();
+
+        assertEquals(10, deposit);
+    }
+
+    @Test
+    public void displaysTransactionHistoryCredit() throws Exception{
+        bankAccount.addingMoneyToAccount(10);
+        bankAccount.creditingMoneyFromTheAccount(5);
+
+        int credit = bankAccount.getCreditTransactions();
+
+        assertEquals(5, credit);
+    }
 
 }
